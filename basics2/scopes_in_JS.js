6c1d909745e5defd12a5d62a9a22b1e96f72c63e
jatinbaniya13ix <101296585+jatinbaniya13ix@users.noreply.github.com>
2024-08-01 06:28:50 +0000

    hositing

--- basics2/scopes_in_JS.js
+++ basics2/scopes_in_JS.js
@@ -9,3 +9,42 @@ function one() {
    
 }
 one()       //if we do not call one()here then nothing will show on screen 
+
+
+
+//suppose i have 2 funtions 
+
+// function addone(num) {
+//     return num+1;
+// }
+// console.log(addone(5));      //here it will give clear answer 6
+
+// const addtwo=function (num) {
+//     return num+2;
+// }
+// console.log(addtwo(5))          //here it will give clear answer 7
+
+
+
+
+//**********interesting thing is************** // 
+
+console.log(addone(5));  //it will also work properly
+
+function addone(num) {  
+    return num+1;
+}
+     
+console.log(addtwo(5))       //it will create issue 
+
+const addtwo= function (num) {
+    return num+2;
+}
+  
+
+//this is something we can say hoisting but its basic .. like how do we store in js
+
+
+
+
+
