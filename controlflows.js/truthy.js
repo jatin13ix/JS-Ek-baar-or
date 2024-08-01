0b03502d9ee882327cc37ecd78c3d991f6db2665
jatinbaniya13ix <101296585+jatinbaniya13ix@users.noreply.github.com>
2024-08-01 10:45:28 +0000

    nullish operator and turiniary operators

--- controlflows.js/truthy.js
+++ controlflows.js/truthy.js
@@ -19,8 +19,17 @@ const emptyarray=[]
             console.log(`I am empty OBj`);  // this is good practice
     }
 
+//nullish coelescing operator [check spelling]
+// so what happens is sometime we dont get direct response ,/
+//  we get 2 values so to handle them we have this operator
 
+let val1;
+val1=null??10;   //Here we dont get null as output 
+console.log(val1);
 
+//it Looks like teriniary operators but they are completely different 
 
+const age=18;                               //these are teriniary operators
+console.log(age<18?"you are teen":"you are adult");
 
 
