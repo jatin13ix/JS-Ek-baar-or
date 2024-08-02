1bda7b50dc23dbb1fc6519b885d5e2ff5aa91210
jatinbaniya13ix <101296585+jatinbaniya13ix@users.noreply.github.com>
2024-08-02 06:58:38 +0000

    forEach

---
+++ controlflows.js/foreachloop.js
@@ -0,0 +1,11 @@
+//The most intersting and usable LoOp
+
+const array=["js","Cpp","ruby","python"] //passes callbackFn
+
+// array.forEach((items)=> console.log(items))  //arrow function 
+
+function printme(items) {
+    // console.log(items);  //predefined function
+     
+}
+array.forEach(printme)  //just pass the method 
