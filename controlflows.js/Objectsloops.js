055563334d7aad7586f3f6031159a2a4d36e25f0
jatinbaniya13ix <101296585+jatinbaniya13ix@users.noreply.github.com>
2024-08-02 06:35:23 +0000

    loops for object(for in loop)

---
+++ controlflows.js/Objectsloops.js
@@ -0,0 +1,19 @@
+const myobj={
+    js:'Javascript',
+    cpp:'c++',
+    rb:'ruby',
+}
+//Now forin loop is a good practice to iterate with Objects
+
+for (const key in myobj) {
+    // console.log(key);  //But it just give me the key so what to do
+    
+}
+for(const key in myobj) {
+    // console.log(myobj[key]); //this will give values
+    
+}
+for(const key in myobj) {
+    // console.log(`Full of ${key} is ${myobj[key]}`); //you can get access to both from this
+    
+}
